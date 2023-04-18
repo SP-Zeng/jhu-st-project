026b9289cb144b92a064020243ae4dd9a55b47a1
Yujian(Ken) He <55962680+Kennnnn774@users.noreply.github.com>
2023-04-18 16:50:15 -0400

    update

--- BACKEND/Home-Ecommerce-Backend/src/test/java/LocalAPITest/LocalAPITest.java
+++ BACKEND/Home-Ecommerce-Backend/src/test/java/LocalAPITest/LocalAPITest.java
@@ -1,23 +1,23 @@
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class LocalApiTest {
-
-    @BeforeClass
-    public static void setup() {
-        // set the base URI for the API
-        baseURI = "http://localhost:4200";
-    }
-
-    @Test
-    public void testApi() {
-        given()
-                .when()
-                .get("/users")
-                .then()
-                .statusCode(200)
-                .body("size()", greaterThan(0));
-    }
-}
+//import static io.restassured.RestAssured.*;
+//import static org.hamcrest.Matchers.*;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//
+//public class LocalApiTest {
+//
+//    @BeforeClass
+//    public static void setup() {
+//        // set the base URI for the API
+//        baseURI = "http://localhost:4200";
+//    }
+//
+//    @Test
+//    public void testApi() {
+//        given()
+//                .when()
+//                .get("/users")
+//                .then()
+//                .statusCode(200)
+//                .body("size()", greaterThan(0));
+//    }
+//}
